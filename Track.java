c8060788b767da749f9cac972393b297e4a7b001
Diego Antonio <diegoantonioalmontezarzuela.com>
2018-01-23 18:17:50 +0100

    Añadido el campo tracks metodos de reiniciar y contador

--- Track.java
+++ Track.java
@@ -13,7 +13,8 @@ public class Track
     private String title;
     // Where the track is stored.
     private String filename;
-    
+    private int playCount;
+
     /**
      * Constructor for objects of class Track.
      * @param artist The track's artist.
@@ -23,8 +24,9 @@ public class Track
     public Track(String artist, String title, String filename)
     {
         setDetails(artist, title, filename);
+        playCount = 0;
     }
-    
+
     /**
      * Constructor for objects of class Track.
      * It is assumed that the file name cannot be
@@ -34,8 +36,9 @@ public class Track
     public Track(String filename)
     {
         setDetails("unknown", "unknown", filename);
+        playCount = 0;
     }
-    
+
     /**
      * Return the artist.
      * @return The artist.
@@ -44,7 +47,7 @@ public class Track
     {
         return artist;
     }
-    
+
     /**
      * Return the title.
      * @return The title.
@@ -53,7 +56,7 @@ public class Track
     {
         return title;
     }
-    
+
     /**
      * Return the file name.
      * @return The file name.
@@ -62,7 +65,7 @@ public class Track
     {
         return filename;
     }
-        
+
     /**
      * Return details of the track: artist, title and file name.
      * @return The track's details.
@@ -71,7 +74,7 @@ public class Track
     {
         return artist + ": " + title + "  (file: " + filename + ")";
     }
-    
+
     /**
      * Set details of the track.
      * @param artist The track's artist.
@@ -84,5 +87,14 @@ public class Track
         this.title = title;
         this.filename = filename;
     }
-    
+
+    public void reset()
+    {
+        playCount = 0;
+    }
+
+    public void incrementplayCount()
+    {
+        playCount ++;
+    }
 }
